c02370e7c7078fe9288247eee6baf2d79665b343
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-09-25 08:29:30 -0300

    Refactor shapes tests using Go interfaces

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -6,6 +6,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type Shape interface {
+	Area() float64
+}
+
+
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := rectangle.Perimeter()
@@ -16,26 +21,23 @@ func TestPerimeter(t *testing.T) {
 
 
 func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		assert.Equal(t, want, got, "got %.2f want %.2f", got, want)
+	}
+
 	t.Run("Test Area with positive values", func(t *testing.T) {
 		rectangle := Rectangle{2.0, 2.0}
-		got := rectangle.Area()
-		want := 4.0
-
-		assert.Equal(t, want, got, "got %.2f want %.2f", got, want)
+		checkArea(t, rectangle, 4.0)
 	});
 	t.Run("Test Area with negative values", func(t *testing.T) {
 		rectangle := Rectangle{-1.0, -1.0}
-		got := rectangle.Area()
-		want := 1.0
-
-		assert.Equal(t, want, got, "got %.2f want %.2f", got, want)
+		checkArea(t, rectangle, 1.0)
 	});
 
 	t.Run("Test Area with circles", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		assert.Equal(t, want, got, "got %g want %g", got, want)
+		checkArea(t, circle, 314.1592653589793)
 	});
 }
