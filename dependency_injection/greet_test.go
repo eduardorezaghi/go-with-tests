8477559524c98da4e225f5e8e97fb9030e38d96e
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-10-02 08:32:42 -0300

    Add basic Dependency Injection examples

---
+++ dependency_injection/greet_test.go
@@ -0,0 +1,24 @@
+package dependencyinjection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	assertCorrectMessage(t, got, want)
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
